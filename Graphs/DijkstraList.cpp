9c4b3113bbd205c2bcb53a66ef1237a460ae7037
wojtek717 <wojciechkonury@gmail.com>
2019-04-18 10:41:48 +0200

    fix v2

--- Graphs/DijkstraList.cpp
+++ Graphs/DijkstraList.cpp
@@ -22,10 +22,8 @@ bool DijkstraList::FormAdjList(std::string fileName) {
     file >> verticesAmmount;
     file >> edges;
 
-    /* To w jakas funcke z pliku */
-    for(int i = 0; i < verticesAmmount; i++)
-    {
-        // Create a vector to represent a row, and add it to the adjList.
+    for(int i = 0; i < verticesAmmount; ++i) {
+        // Stworz vector reprezeentujacy wiersz i dodaj go do listy
         std::vector<std::pair<int, int> > row;
         adjList.push_back(row);
     }
@@ -35,63 +33,53 @@ bool DijkstraList::FormAdjList(std::string fileName) {
         file >> destination;
         file >> cost;
 
+        // Polaczenie w dwie strony
         adjList[source].push_back(std::make_pair(destination, cost));
         adjList[destination].push_back(std::make_pair(source, cost));
     }
 
     return true;
-
-
-
 }
 
 void DijkstraList::DijkstraSP(int &start) {
-    std::cout << "\nGetting the shortest path from " << start << " to all other nodes.\n";
     Vertex vertex;
 
-    // Initialize all source->vertex as infinite.
+    // Zainicjalizuj wszystkie odleglosci jako inf
     int n = adjList.size();
     for(int i = 0; i < n; i++)
     {
-        //dist.push_back(INFINITY); // Define "infinity" as necessary by constraints.
         vertex.index = i;
         vertices.push_back(vertex);
     }
 
-    // Create a PQ.
     std::priority_queue<std::pair<int, int>, std::vector< std::pair<int, int> >, std::greater<std::pair<int, int> > > pq;
 
-    // Add source to pq, where distance is 0.
+    // Odleglosc od startu do startu zawsze = 0
     pq.push(std::make_pair(start, 0));
     vertices[start].distance = 0;
 
-    // While pq isn't empty...
-    while(pq.empty() == false)
+
+    while(!pq.empty())
     {
-        // Get min distance vertex from pq. (Call it u.)
+        // Dostan najblizszy wierzcholek
         int u = pq.top().first;
         pq.pop();
 
-        // Visit all of u's friends. For each one (called v)....
+        // Sprawdz pozostale wierzcholki
         for(int i = 0; i < adjList[u].size(); i++)
         {
             int v = adjList[u][i].first;
             int weight = adjList[u][i].second;
 
-            // If the distance to v is shorter by going through u...
+            // Jezeli odleglosc do v jest mniejsza idac przez u
             if(vertices[v].distance > vertices[u].distance + weight)
             {
-                // Update the distance of v.
                 vertices[v].distance = vertices[u].distance + weight;
-                // Insert v into the pq.
                 pq.push(std::make_pair(v, vertices[v].distance));
-
                 vertices[v].parent = &vertices[u];
             }
         }
     }
-
-
 
     PrintShortestPath(start);
 }
